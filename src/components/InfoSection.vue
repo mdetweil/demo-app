4c272596d458f9ebbb0504984c9a19486b120bf4
mdetweiler <mdetweil@gmail.com>
2020-10-11 14:58:58 -0500

    update to use v-model

--- src/components/InfoSection.vue
+++ src/components/InfoSection.vue
@@ -3,13 +3,13 @@
     <v-card-title>Info Section</v-card-title>
     <v-container>
       <v-row>
-        <v-text-field label="First Name" id="firstName" :value="value.firstName.value" @input="value.firstName.value = $event" outlined />
+        <v-text-field label="First Name" id="firstName" v-model="value.firstName.value" outlined />
       </v-row>
       <v-row>
-        <v-text-field label="Middle Name" id="middleName" :value="value.middleName.value" @input="value.middleName.value = $event" outlined />
+        <v-text-field label="Middle Name" id="middleName" v-model="value.middleName.value" outlined />
       </v-row>
       <v-row>
-        <v-text-field label="Last Name" id="lastName" :value="value.lastName.value" @input="value.lastName.value = $event" outlined />
+        <v-text-field label="Last Name" id="lastName" v-model="value.lastName.value" outlined />
       </v-row>
     </v-container>
   </v-card>
